a1ca9d20c4e57dab56f06fd6323a979c8ec8e32a
hossainarif37 <hossainarif4937@gmail.com>
2024-02-07 01:51:24 +0600

    fix: Fix responsive issue of Navbar with Login and Signup component

--- components/ui/Modal/LoginModal.tsx
+++ components/ui/Modal/LoginModal.tsx
@@ -7,8 +7,8 @@ const LoginModal = () => {
     const { loginModal, setLoginModal } = useModalContext();
 
     return (
-        <div className={`fixed z-50 w-full h-full bg-black bg-opacity-30 flex justify-center items-center top-0 right-0 ${loginModal ? 'scale-100' : 'scale-0'}`}>
-            <div className={`lg:w-[450px] bg-white rounded-lg text-black relative p-5 duration-300 ${loginModal ? 'scale-100' : 'scale-0'}`}>
+        <div className={`fixed px-2 md:px-0 z-50 w-full h-full bg-black bg-opacity-30 flex justify-center items-center top-0 right-0 ${loginModal ? 'scale-100' : 'scale-0'}`}>
+            <div className={`md:w-[450px] w-full bg-white rounded-lg text-black relative p-5 duration-300 ${loginModal ? 'scale-100' : 'scale-0'}`}>
                 <LoginForm />
             </div>
         </div>
