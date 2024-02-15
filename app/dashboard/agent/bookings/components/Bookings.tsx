3b041df550e15ef838448b118331f3c3af59ef17
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2024-02-15 09:35:14 +0600

    fix: booking details page for all user roles

--- app/dashboard/agent/bookings/components/Bookings.tsx
+++ app/dashboard/agent/bookings/components/Bookings.tsx
@@ -9,6 +9,7 @@ import { FaShoppingBag } from 'react-icons/fa';
 import { formatDistanceToNow } from 'date-fns';
 import { getBookingsById } from '@/lib/database/getUserBooking';
 import { useAuthContext } from '@/providers/AuthProvider';
+import Link from 'next/link';
 
 
 interface BookingsProps {
@@ -16,7 +17,7 @@ interface BookingsProps {
 }
 const Bookings = () => {
     type booking = {
-        id: number;
+        _id: number;
         price: string;
         total: number;
         status: string;
@@ -70,27 +71,29 @@ const Bookings = () => {
             <div className='flex justify-between px-4 pt-4'>
                 <h2>bookings</h2>
             </div>
-            {bookings?.length > 0 && <div className='p-4'>
+            <div className='p-4'>
                 <div className='w-full  m-auto p-4 rounded-lg overflow-y-auto overflow-x-auto'>
-                    <div className='my-3 bg-[#2E374A] p-5 rounded-t-xl grid md:grid-cols-5 sm:grid-cols-3 grid-cols-2 items-center justify-between cursor-pointer font-semibold'>
-                        <span>Name</span>
+                    <div className='my-3 bg-[#2E374A] p-5 rounded-t-xl grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 items-center justify-between cursor-pointer font-semibold'>
+                        <span>booking</span>
                         <span className='sm:text-left text-right'>Status</span>
                         <span className='hidden md:grid'>Last booking</span>
-                        <span className='hidden sm:flex'>Check in <span className='px-6 inline-block'>-</span> Check out</span>
-                        <span className='hidden md:grid text-center'>Total Price</span>
+                        <span className='hidden sm:flex'>Check in <span className='px-8 inline-block'>-</span> Check out</span>
                     </div>
                     <ul>
-                        {bookings?.slice()?.reverse()?.map((booking: booking, id: number | string) => (
+                        {bookings.slice().reverse().map((booking: booking, id: number) => (
                             <li
                                 key={id}
-                                className=' hover:bg-[#2E374A] bg-primary-50 rounded-lg my-3 p-2 grid md:grid-cols-5 sm:grid-cols-3 grid-cols-2 items-center justify-between cursor-pointer'
+                                className=' hover:bg-[#2E374A] bg-primary-50 rounded-lg my-3 p-2 grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 items-center justify-between cursor-pointer'
                             >
                                 <div className='flex'>
                                     <div className='bg-purple-100 p-3 rounded-lg'>
                                         <FaShoppingBag className='text-purple-800' />
                                     </div>
-                                    <div className='pl-4 flex items-center font-semibold'>
-                                        <p>{user?.name}</p>
+                                    <div className='pl-4'>
+                                        <p className=' font-bold'>
+                                            ${booking.price}
+                                        </p>
+                                        <p className=' text-sm'>{user?.name}</p>
                                     </div>
                                 </div>
                                 <button className=' sm:text-left text-right md:text-sm text-xs'>
@@ -117,19 +120,21 @@ const Bookings = () => {
                                         {format(new Date(booking.endDate), "MMM dd, yyyy")}
                                     </p>
                                     {/* <BsThreeDotsVertical /> */}
+                                    <Link href={`/dashboard/agent/booking-details/${booking._id}`}>
+                                        <button className=' sm:text-left text-right md:text-sm text-xs'>
+                                            <span
+                                                className={'bg-secondary-50 text-primary-50 p-2 rounded-md underline'}
+                                            >
+                                                Details
+                                            </span>
+                                        </button>
+                                    </Link>
                                 </div>
-                                <p className='text-center font-semibold'>
-                                    $ {booking.price}
-                                </p>
                             </li>
                         ))}
                     </ul>
                 </div>
-            </div>}
-
-            {
-                bookings?.length < 1 && <div className="text-center mt-20 text-white"><h1 className="lg:text-4xl text-2xl text-center">You haven't any Booking yet. Please booking Property</h1></div>
-            }
+            </div>
         </div>
     );
 };
