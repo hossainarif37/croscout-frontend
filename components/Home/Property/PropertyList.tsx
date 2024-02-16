16f13f53852e684653816d5a1882e7cccce6590e
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2024-02-16 12:45:01 +0600

    fix property list responsive issue

--- components/Home/Property/PropertyList.tsx
+++ components/Home/Property/PropertyList.tsx
@@ -85,7 +85,7 @@ const PropertyList = () => {
                 /></div>
             }
 
-            <div className="grid grid-cols-1 lg:grid-cols-4 gap-5">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
                 {properties?.map((property: Property, index: number) => (
                     // <Link
                     //     href={`/property-details/${index + 1}`}
