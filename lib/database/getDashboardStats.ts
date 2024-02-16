b3bf5342b225a23d078dbb8fbbe591d9ae24d48d
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2024-02-16 13:10:27 +0600

    add: get dashboard stats api

---
+++ lib/database/getDashboardStats.ts
@@ -0,0 +1,10 @@
+export const getDashboardStats = async ({ id }: { id: string }) => {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/dashboard/stats/${id}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+    const stats = await response.json();
+    return stats;
+}
