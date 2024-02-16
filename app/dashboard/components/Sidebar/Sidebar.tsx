5d480c62ee3039de761a6a14302414efc5549c8f
Hridoy2206 <ridoyahmed2206@gmail.com>
2024-02-17 03:25:33 +0600

    fix: profile image size, responsive calendar and table design

--- app/dashboard/components/Sidebar/Sidebar.tsx
+++ app/dashboard/components/Sidebar/Sidebar.tsx
@@ -195,7 +195,7 @@ export default function Sidebar() {
         <div>
             <div className={`${styles.container}`}>
                 <div className='flex gap-4 items-center mb-4'>
-                    <Image src={user?.image || userImg} alt='userImage' width={50} height={50} className='rounded-full border-white border' />
+                    <Image src={user?.image || userImg} alt='userImage' width={200} height={100} className='rounded-full w-14 h-14 border-white border' />
                     <div className='flex flex-col'>
                         <span>{user?.name}</span>
                         <span className='text-sm text-gray-300'>{user?.role}</span>
